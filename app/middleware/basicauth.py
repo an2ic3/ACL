eee1ebc9de36f1b0f719fc4a7220b4613599daff
Paul-Brauckmann <paul.brauckmann@outlook.com>
2020-10-28 17:21:22 +0100

    fix basic auth

--- app/middleware/basicauth.py
+++ app/middleware/basicauth.py
@@ -1,9 +1,13 @@
 import base64
 import binascii
+import logging
 
 from django.http import HttpResponse, HttpRequest, HttpResponseBadRequest
 from django.urls import reverse
 from django.contrib.auth import authenticate
+
+
+logger = logging.getLogger(__name__)
 
 
 class BasicAuthMiddleware:
@@ -18,14 +22,16 @@ class BasicAuthMiddleware:
                 continue
             return self.get_response(request)
 
+        if not (auth_header := request.headers.get('Authorization')):
+            response = HttpResponse(status=401)
+            response['WWW-Authenticate'] = 'Basic realm="ACL"'
+            return response
+
         try:
-            if not (auth_header := request.headers.get('Authorization', None)):
-                response = HttpResponse(status=401)
-                response['WWW-Authenticate'] = 'Basic realm="ACL"'
-                return response
             _, basic_auth = auth_header.split(' ')
-            username, passwd = base64.b64decode(basic_auth).decode().split(':')
-        except (ValueError, binascii.Error):
+            username, passwd = base64.b64decode(basic_auth).decode().split(':', maxsplit=1)
+        except (ValueError, binascii.Error) as e:
+            logger.error(e)
             return HttpResponseBadRequest()
 
         user = authenticate(request, username=username, password=passwd)
